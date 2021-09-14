b5f2db13aed08fb048e42ea8bf6edafcc06415f6
strawhatcrue1 <84302030+strawhatcrue1@users.noreply.github.com>
2021-09-15 03:19:07 +0700

    Layout UI

--- app/src/main/java/com/example/listofusers/MainActivity.java
+++ app/src/main/java/com/example/listofusers/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.listofusers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
